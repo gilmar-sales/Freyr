cc9bb7d798e1b7c4c9dbc9c525d6044cfb121806
gilmar-sales <gilmar.custodio@outlook.com.br>
2026-04-21 15:16:45 -0300

    chore: update skirnir to 0.16.0

--- src/Core/FreyrExtension.cpp
+++ src/Core/FreyrExtension.cpp
@@ -2,29 +2,29 @@
 
 namespace FREYR_NAMESPACE
 {
-    void FreyrExtension::ConfigureServices(skr::ServiceCollection& services)
+    void FreyrExtension::ConfigureServices(Ref<skr::ServiceCollection> services)
     {
         for (auto& func : mServiceCollectionFunctions)
         {
-            func(services);
+            func(*services);
         }
 
-        services.AddSingleton<FreyrOptions>(mFreyrOptionsBuilder.Build());
-        services.AddSingleton<EntityManager>();
-        services.AddSingleton<ComponentManager>();
-        services.AddSingleton<SystemManager>();
-        services.AddSingleton<ThreadPool>();
-        services.AddSingleton<TaskCounter>();
-        services.AddSingleton<EventManager>();
-        services.AddSingleton<QueryAggregator>();
-        services.AddSingleton<Scene>();
-        services.AddTransient<Archetype>();
-        services.AddTransient<Query>();
+        services->AddSingleton<FreyrOptions>(mFreyrOptionsBuilder.Build());
+        services->AddSingleton<EntityManager>();
+        services->AddSingleton<ComponentManager>();
+        services->AddSingleton<SystemManager>();
+        services->AddSingleton<ThreadPool>();
+        services->AddSingleton<TaskCounter>();
+        services->AddSingleton<EventManager>();
+        services->AddSingleton<QueryAggregator>();
+        services->AddSingleton<Scene>();
+        services->AddTransient<Archetype>();
+        services->AddTransient<Query>();
     }
 
-    void FreyrExtension::UseServices(skr::ServiceProvider& serviceProvider)
+    void FreyrExtension::UseServices(Ref<skr::ServiceProvider> serviceProvider)
     {
-        const auto systemManager = serviceProvider.GetService<SystemManager>();
+        const auto systemManager = serviceProvider->GetService<SystemManager>();
 
         for (const auto& config : mPipelineConfigs)
         {
@@ -36,7 +36,7 @@ namespace FREYR_NAMESPACE
             func(*systemManager);
         }
 
-        const auto componentManager = serviceProvider.GetService<ComponentManager>();
+        const auto componentManager = serviceProvider->GetService<ComponentManager>();
 
         for (auto& func : mComponentManagerFunctions)
         {
